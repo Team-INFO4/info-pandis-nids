bd0cea67ae8e25cd2d46f602d1bb3377f5c4eb1e
kmj36 <kmj36953695@gmail.com>
2022-05-21 14:58:29 +0900

    [ADD] 대화상자창 2개 추가

---
+++ PandisNIDS/PandisNIDS/resource.h
@@ -0,0 +1,36 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// PandisNIDS.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PANDISNIDS_DIALOG           102
+#define IDD_DIALOG_PANDISNIDS           102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_DEVICE_SELECT        131
+#define IDD_DIALOG_RULE_SET             133
+#define IDC_EDIT_DISPLAY                1000
+#define IDC_BUTTON_START                1001
+#define IDC_BUTTON_STOP                 1002
+#define IDC_BUTTON_PAUSE                1003
+#define IDC_LIST1                       1004
+#define IDC_EDIT_SELECT_TRANSPARENC     1005
+#define IDC_SPIN1                       1006
+#define IDC_COMBO_RULE_PROTOCOL_SEL     1008
+#define IDC_IPADDRESS1                  1009
+#define IDC_EDIT_RULE_SOURCE_PORT       1010
+#define IDC_CHECK1                      1011
+#define IDC_CHECK_RULE_SOURCE_ANY_IP    1011
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        136
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1012
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
