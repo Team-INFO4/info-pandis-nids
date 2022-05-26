a4446ac19c8e1eb8a36eb8e2803d7adfda6c5c9b
sleepanda <jusuck4523@gmail.com>
2022-05-26 11:46:27 +0900

    [EDIT] add ARP and Ipv4, Ipv6

--- PandisNIDS/PandisNIDS/WinProtocols.cpp
+++ PandisNIDS/PandisNIDS/WinProtocols.cpp
@@ -59,6 +59,7 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.eth)
 	{
+		strPrintString.AppendFormat(_T("Ethernet Layer\r\n"));
 		strPrintString.AppendFormat(_T("Source MAC Address:	%02X:%02X:%02X:%02X:%02X:%02X\r\n"),
 			packet_headers.eth->eth_src_mac[0],
 			packet_headers.eth->eth_src_mac[1],
@@ -80,17 +81,70 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.arph)
 	{
+		strPrintString.AppendFormat(_T("ARP protocol \r\n"));
+		strPrintString.AppendFormat(_T("Source IP Address:  %3D.%3D.%3D.%3D\r\n"), 
+			packet_headers.arph->arp_src_ip[0],
+			packet_headers.arph->arp_src_ip[1],
+			packet_headers.arph->arp_src_ip[2],
+			packet_headers.arph->arp_src_ip[3]);
+		strPrintString.AppendFormat(_T("->   Source MAC Address:	%02X:%02X:%02X:%02X:%02X:%02X\r\n"),
+			packet_headers.arph->arp_src_mac[0],
+			packet_headers.arph->arp_src_mac[1],
+			packet_headers.arph->arp_src_mac[2],
+			packet_headers.arph->arp_src_mac[3],
+			packet_headers.arph->arp_src_mac[4],
+			packet_headers.arph->arp_src_mac[5]);
 
+		strPrintString.AppendFormat(_T("Destination IP Address:  %3D\.%3D\.%3D\.%3D\r\n"),
+			packet_headers.arph->arp_dst_ip[0],
+			packet_headers.arph->arp_dst_ip[1],
+			packet_headers.arph->arp_dst_ip[2],
+			packet_headers.arph->arp_dst_ip[3]);
+		strPrintString.AppendFormat(_T("->   Destination MAC Address:	%02X:%02X:%02X:%02X:%02X:%02X\r\n"),
+			packet_headers.arph->arp_dst_mac[0],
+			packet_headers.arph->arp_dst_mac[1],
+			packet_headers.arph->arp_dst_mac[2],
+			packet_headers.arph->arp_dst_mac[3],
+			packet_headers.arph->arp_dst_mac[4],
+			packet_headers.arph->arp_dst_mac[5]);
 	}
 
 	if (packet_headers.ip4h)
 	{
-
+		strPrintString.AppendFormat(_T("Ipv4 Protocal \r\n"));
+		strPrintString.AppendFormat(_T("Source IP Address:   %3D\.%3D\.%3D\.%3D\r\n"),
+			packet_headers.ip4h->ip4_src_ip[0],
+			packet_headers.ip4h->ip4_src_ip[1],
+			packet_headers.ip4h->ip4_src_ip[2],
+			packet_headers.ip4h->ip4_src_ip[3]);
+		strPrintString.AppendFormat(_T("Destination IP Address:   %3D\.%3D\.%3D\.%3D\r\n"),
+			packet_headers.ip4h->ip4_dst_ip[0],
+			packet_headers.ip4h->ip4_dst_ip[1],
+			packet_headers.ip4h->ip4_dst_ip[2],
+			packet_headers.ip4h->ip4_dst_ip[3]);
 	}
 
 	if (packet_headers.ip6h)
 	{
-
+		strPrintString.AppendFormat(_T("Ipv6 Protocal \r\n"));
+		strPrintString.AppendFormat(_T("Source IP Address:   %4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x\r\n"),
+			packet_headers.ip6h->ip6_src_ip[0], 
+			packet_headers.ip6h->ip6_src_ip[1], 
+			packet_headers.ip6h->ip6_src_ip[2], 
+			packet_headers.ip6h->ip6_src_ip[3], 
+			packet_headers.ip6h->ip6_src_ip[4], 
+			packet_headers.ip6h->ip6_src_ip[5], 
+			packet_headers.ip6h->ip6_src_ip[6], 
+			packet_headers.ip6h->ip6_src_ip[7]);
+		strPrintString.AppendFormat(_T("Destination IP Address:   %4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x\r\n"),
+			packet_headers.ip6h->ip6_dst_ip[0],
+			packet_headers.ip6h->ip6_dst_ip[1],
+			packet_headers.ip6h->ip6_dst_ip[2],
+			packet_headers.ip6h->ip6_dst_ip[3],
+			packet_headers.ip6h->ip6_dst_ip[4],
+			packet_headers.ip6h->ip6_dst_ip[5],
+			packet_headers.ip6h->ip6_dst_ip[6],
+			packet_headers.ip6h->ip6_dst_ip[7]);
 	}
 
 	if (packet_headers.tcph)
