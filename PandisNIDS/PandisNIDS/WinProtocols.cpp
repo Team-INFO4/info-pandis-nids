7e05aeb92e3658f3fc537fbfeb77faf4f3e2d177
kmj36 <kmj36953695@gmail.com>
2022-05-26 14:57:53 +0900

    [MODIFY] 프로토콜별 출력 버그 수정

--- PandisNIDS/PandisNIDS/WinProtocols.cpp
+++ PandisNIDS/PandisNIDS/WinProtocols.cpp
@@ -59,7 +59,7 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.eth)
 	{
-		strPrintString.AppendFormat(_T("Ethernet Layer\r\n"));
+		strPrintString.AppendFormat(_T("[Ethernet Layer]\r\n"));
 		strPrintString.AppendFormat(_T("Source MAC Address:	%02X:%02X:%02X:%02X:%02X:%02X\r\n"),
 			packet_headers.eth->eth_src_mac[0],
 			packet_headers.eth->eth_src_mac[1],
@@ -81,8 +81,8 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.arph)
 	{
-		strPrintString.AppendFormat(_T("ARP protocol \r\n"));
-		strPrintString.AppendFormat(_T("Source IP Address:  %3D.%3D.%3D.%3D\r\n"), 
+		strPrintString.AppendFormat(_T("[ARP protocol]\r\n"));
+		strPrintString.AppendFormat(_T("Source IP Address:  %0d.%0d.%0d.%0d\r\n"),
 			packet_headers.arph->arp_src_ip[0],
 			packet_headers.arph->arp_src_ip[1],
 			packet_headers.arph->arp_src_ip[2],
@@ -95,7 +95,7 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 			packet_headers.arph->arp_src_mac[4],
 			packet_headers.arph->arp_src_mac[5]);
 
-		strPrintString.AppendFormat(_T("Destination IP Address:  %3D\.%3D\.%3D\.%3D\r\n"),
+		strPrintString.AppendFormat(_T("Destination IP Address:  %0d.%0d.%0d.%0d\r\n"),
 			packet_headers.arph->arp_dst_ip[0],
 			packet_headers.arph->arp_dst_ip[1],
 			packet_headers.arph->arp_dst_ip[2],
@@ -111,13 +111,14 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.ip4h)
 	{
-		strPrintString.AppendFormat(_T("Ipv4 Protocal \r\n"));
-		strPrintString.AppendFormat(_T("Source IP Address:   %3D\.%3D\.%3D\.%3D\r\n"),
+		strPrintString.AppendFormat(_T("[Ipv4 Protocol]\r\n"));
+		strPrintString.AppendFormat(_T("Source IP Address:   %0d.%0d.%0d.%0d\r\n"),
 			packet_headers.ip4h->ip4_src_ip[0],
 			packet_headers.ip4h->ip4_src_ip[1],
 			packet_headers.ip4h->ip4_src_ip[2],
 			packet_headers.ip4h->ip4_src_ip[3]);
-		strPrintString.AppendFormat(_T("Destination IP Address:   %3D\.%3D\.%3D\.%3D\r\n"),
+
+		strPrintString.AppendFormat(_T("Destination IP Address:   %0d.%0d.%0d.%0d\r\n"),
 			packet_headers.ip4h->ip4_dst_ip[0],
 			packet_headers.ip4h->ip4_dst_ip[1],
 			packet_headers.ip4h->ip4_dst_ip[2],
@@ -126,54 +127,50 @@ int PrintPacketData(hdr_t packet_headers, CString& strPrintString) // TODO : Ãâ·
 
 	if (packet_headers.ip6h)
 	{
-		strPrintString.AppendFormat(_T("Ipv6 Protocal \r\n"));
+		strPrintString.AppendFormat(_T("[Ipv6 Protocol]\r\n"));
 		strPrintString.AppendFormat(_T("Source IP Address:   %4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x\r\n"),
-			packet_headers.ip6h->ip6_src_ip[0], 
-			packet_headers.ip6h->ip6_src_ip[1], 
-			packet_headers.ip6h->ip6_src_ip[2], 
-			packet_headers.ip6h->ip6_src_ip[3], 
-			packet_headers.ip6h->ip6_src_ip[4], 
-			packet_headers.ip6h->ip6_src_ip[5], 
-			packet_headers.ip6h->ip6_src_ip[6], 
-			packet_headers.ip6h->ip6_src_ip[7]);
+			ntohs(packet_headers.ip6h->ip6_src_ip[0]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[1]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[2]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[3]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[4]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[5]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[6]),
+			ntohs(packet_headers.ip6h->ip6_src_ip[7]));
+
 		strPrintString.AppendFormat(_T("Destination IP Address:   %4x:%4x:%4x:%4x:%4x:%4x:%4x:%4x\r\n"),
-			packet_headers.ip6h->ip6_dst_ip[0],
-			packet_headers.ip6h->ip6_dst_ip[1],
-			packet_headers.ip6h->ip6_dst_ip[2],
-			packet_headers.ip6h->ip6_dst_ip[3],
-			packet_headers.ip6h->ip6_dst_ip[4],
-			packet_headers.ip6h->ip6_dst_ip[5],
-			packet_headers.ip6h->ip6_dst_ip[6],
-			packet_headers.ip6h->ip6_dst_ip[7]);
+			ntohs(packet_headers.ip6h->ip6_dst_ip[0]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[1]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[2]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[3]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[4]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[5]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[6]),
+			ntohs(packet_headers.ip6h->ip6_dst_ip[7]));
 	}
 
 	if (packet_headers.tcph)
 	{
-		strPrintString.AppendFormat(_T("tcp Protocal \r\n"));
-		strPrintString.AppendFormat(_T("Source Port:   %5d \r\n"),
-			packet_headers.tcph->tcp_src_port);
-		strPrintString.AppendFormat(_T("Destination Port:   %5d \r\n"),
-			packet_headers.tcph->tcp_dst_port);
+		strPrintString.AppendFormat(_T("[tcp Protocol]\r\n"));
+		strPrintString.AppendFormat(_T("Source Port:   %5d \r\n"), ntohs(packet_headers.tcph->tcp_src_port));
+
+		strPrintString.AppendFormat(_T("Destination Port:   %5d \r\n"), ntohs(packet_headers.tcph->tcp_dst_port));
 	}
 
 	if (packet_headers.udph)
 	{
-		strPrintString.AppendFormat(_T("udp Protocal \r\n"));
-		strPrintString.AppendFormat(_T("Source Port:   %5d \r\n"),
-			packet_headers.udph->udp_src_port);
-		strPrintString.AppendFormat(_T("Destination Port:   %5d \r\n"),
-			packet_headers.udph->udp_dst_port);
+		strPrintString.AppendFormat(_T("[udp Protocol]\r\n"));
+		strPrintString.AppendFormat(_T("Source Port:   %5d \r\n"), ntohs(packet_headers.udph->udp_src_port));
+		strPrintString.AppendFormat(_T("Destination Port:   %5d \r\n"), ntohs(packet_headers.udph->udp_dst_port));
 	}
 
 	if (packet_headers.icmph)
 	{
-		strPrintString.AppendFormat(_T("icmp Protocal \r\n"));
-		strPrintString.AppendFormat(_T("icmp type:   0x%04X \r\n"),
-			ntohs(packet_headers.icmph->icmp_type));
-		strPrintString.AppendFormat(_T("icmp code:   0x%04X \r\n"),
-			ntohs(packet_headers.icmph->icmp_code));
+		strPrintString.AppendFormat(_T("[icmp Protocol]\r\n"));
+		strPrintString.AppendFormat(_T("icmp type:   %d \r\n"), packet_headers.icmph->icmp_type);
+		strPrintString.AppendFormat(_T("icmp code:   %d \r\n"), packet_headers.icmph->icmp_code);
 	}
 
-	strPrintString += "/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-/\r\n";
+	strPrintString += "/*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-/\r\n\r\n";
 	return 0;
 }
